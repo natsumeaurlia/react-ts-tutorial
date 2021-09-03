82b2484e5bd5677fbb705131d8264dcfad577715
natsume0718 <moto718program@gmail.com>
2021-09-03 23:57:23 +0900

    add

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Button } from './Button';
+import { Button } from '../components/atoms/Button';
 
 export default {
-  title: 'Example/Button',
+  title: 'atoms/Button',
   component: Button,
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -13,25 +13,62 @@ export default {
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
+export const Red = Template.bind({});
+Red.args = {
+  color: 'red',
+  children: <span style={{ color: 'white' }}>はろー</span>,
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
+export const Blue = Template.bind({});
+Blue.args = {
+  color: 'blue',
+  children: <span style={{ color: 'white' }}>はろー</span>,
 };
 
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
+export const Green = Template.bind({});
+Green.args = {
+  color: 'green',
+  children: <span style={{ color: 'white' }}>はろー</span>,
 };
 
 export const Small = Template.bind({});
 Small.args = {
+  color: 'red',
   size: 'small',
-  label: 'Button',
+  children: <span style={{ color: 'white' }}>はろー</span>,
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  color: 'red',
+  size: 'medium',
+  children: <span style={{ color: 'white' }}>はろー</span>,
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  color: 'red',
+  size: 'large',
+  children: <span style={{ color: 'white' }}>はろー</span>,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  color: 'red',
+  disabled: true,
+  children: <span style={{ color: 'white' }}>はろー</span>,
+};
+
+export const Apply = Template.bind({});
+Apply.args = {
+  color: 'blue',
+  children: <span style={{ color: 'white' }}>応募する</span>,
+};
+
+export const Delete = Template.bind({});
+Delete.args = {
+  color: 'red',
+  disabled: true,
+  size: 'small',
+  children: <span style={{ color: 'white' }}>削除する</span>,
 };
